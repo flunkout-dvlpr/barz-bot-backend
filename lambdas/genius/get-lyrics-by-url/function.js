776a5dc5a8845b1ca86b0c4eda7c421afb115002
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-25 16:30:07 -0500

    Get data from songURL(body) and split/clean up text and return response

--- lambdas/genius/get-lyrics-by-url/function.js
+++ lambdas/genius/get-lyrics-by-url/function.js
@@ -1,8 +1,28 @@
-exports.handler = async (event) => {
-  console.log(event);
-  axios.get('https://genius.com/Aaron-may-temporary-lyrics').then((response) => {
-    console.log(response);
-  };
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-  return event;
+exports.handler = async (event) => {
+  console.log(event)
+  let lyrics;
+  await axios.get('https://genius.com/Aaron-may-temporary-lyrics').then((response) => {
+    const $ = cheerio.load(response.data)
+    lyrics = $('div[class="lyrics"]').text().trim()
+  });
+
+  if (lyrics) {
+    lyrics = lyrics.split(/\r\n|\r|\n/)
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      payload: lyrics,
+      type: "success",
+      message: "Loaded all invoices successfully"
+    })
+  };
 };
