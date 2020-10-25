409ed44f3c64920d31bc04ef4427105497e80a81
flunkout-dvlpr <info@juliogonzalez.space>
2020-10-25 17:55:59 -0500

    Handle certain cases when genius scraping cant find lyrics from song url

--- lambdas/genius/get-lyrics-by-url/function.js
+++ lambdas/genius/get-lyrics-by-url/function.js
@@ -3,6 +3,20 @@ const cheerio = require('cheerio');
 
 exports.handler = async (event) => {
   const songURL = event.body
+  if (songURL.includes('annotated')) {
+    console.log('This one failed')
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        payload: ['Cant Find Lyrics :('],
+        type: "fail"
+      })
+    };
+  }
   let lyrics;
   await axios.get(songURL).then((response) => {
     const $ = cheerio.load(response.data)
@@ -21,8 +35,7 @@ exports.handler = async (event) => {
     },
     body: JSON.stringify({
       payload: lyrics,
-      type: "success",
-      message: "Loaded all invoices successfully"
+      type: "success"
     })
   };
 };
